77b76a7e9eec4b1e3c620d11853eb3ceb1d82dbf
X27 <87399193+GamerX27@users.noreply.github.com>
2025-03-13 23:54:40 +0100

    Update Docker-Install.sh

--- Docker/Docker-Install.sh
+++ Docker/Docker-Install.sh
@@ -47,7 +47,7 @@ install_portainer() {
   echo "Running Portainer CE container..."
   sudo docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always \
     -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data \
-    portainer/portainer-ce:2.21.1
+    portainer/portainer-ce:latest
 
   if [ $? -eq 0 ]; then
     print_success "Portainer CE installed successfully!"
